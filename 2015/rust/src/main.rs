033dcd59bab8b493dd7602b6cc517008ec2cb55c
yevhen orlov <yevhenorlov@gmail.com>
2022-12-18 14:06:06 +0100

    rust 2015.0102

--- 2015/rust/src/main.rs
+++ 2015/rust/src/main.rs
@@ -1,9 +1,11 @@
 pub mod solutions;
 
 use crate::solutions::s01::s0101;
+use crate::solutions::s01::s0102;
 
 const INPUT_1: &str = "input/01.txt";
 
 fn main() {
-    s0101(INPUT_1);
+    println!("{:?}", s0101(INPUT_1));
+    println!("{:?}", s0102(INPUT_1));
 }
