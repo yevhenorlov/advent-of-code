6340570c7def1264e28e35ab403de121205caefe
yevhen orlov <yevhenorlov@gmail.com>
2025-06-05 23:06:02 +0200

    0302

--- go/2015/day03/main.go
+++ go/2015/day03/main.go
@@ -26,15 +26,31 @@ import (
 	"os"
 )
 
-type SantasList struct {
+type Santa struct {
+	coords  [2]int
 	Visited map[string]struct{}
 }
 
-func (s *SantasList) Visit(coords []int) {
+func (s *Santa) Walk(direction rune) {
+	switch direction {
+	case '<':
+		s.coords[0] = s.coords[0] - 1
+	case '>':
+		s.coords[0] = s.coords[0] + 1
+	case 'v':
+		s.coords[1] = s.coords[1] - 1
+	case '^':
+		s.coords[1] = s.coords[1] + 1
+	default:
+		break
+	}
+}
+
+func (s *Santa) Visit() {
 	if s.Visited == nil {
 		s.Visited = make(map[string]struct{})
 	}
-	key := fmt.Sprintf("%v, %v", coords[0], coords[1])
+	key := fmt.Sprintf("%v, %v", s.coords[0], s.coords[1])
 	s.Visited[key] = struct{}{}
 }
 
@@ -46,9 +62,10 @@ func partOne() {
 	}
 
 	reader := bufio.NewReader(file)
-	coords := []int{0, 0}
-	santasList := &SantasList{}
-	santasList.Visit(coords) // visit first house before beginning
+	santa := Santa{
+		coords: [2]int{0, 0},
+	}
+	santa.Visit() // visit first house before beginning
 	for {
 		char, _, err := reader.ReadRune()
 		if err == io.EOF {
@@ -57,30 +74,81 @@ func partOne() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		switch char {
-		case '<':
-			coords[0] = coords[0] - 1
-		case '>':
-			coords[0] = coords[0] + 1
-		case 'v':
-			coords[1] = coords[1] - 1
-		case '^':
-			coords[1] = coords[1] + 1
-		default:
-			break
-		}
-		santasList.Visit(coords)
+		santa.Walk(char)
+		santa.Visit()
 	}
 
-	fmt.Println("total houses visited:", len(santasList.Visited))
+	fmt.Println("total houses visited:", len(santa.Visited))
 }
 
 /*
-PART 2 Task
+
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
 */
 
+func countUniqueKeys(set1, set2 map[string]struct{}) int {
+	unique := make(map[string]struct{})
+
+	for key := range set1 {
+		unique[key] = struct{}{}
+	}
+	for key := range set2 {
+		unique[key] = struct{}{}
+	}
+	return len(unique)
+}
+
 func partTwo() {
-	//
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(file)
+	santa := Santa{
+		coords: [2]int{0, 0},
+	}
+	roboSanta := Santa{
+		coords: [2]int{0, 0},
+	}
+
+	santa.Visit()     // visit first house before beginning
+	roboSanta.Visit() // visit first house before beginning
+	index := 0
+	for {
+		char, _, err := reader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if index%2 == 0 {
+			santa.Walk(char)
+			santa.Visit()
+		} else {
+			roboSanta.Walk(char)
+			roboSanta.Visit()
+		}
+		index++
+	}
+	fmt.Println("total houses visited by Santa:", len(santa.Visited))
+	fmt.Println("total houses visited by RoboSanta:", len(roboSanta.Visited))
+	fmt.Println("unique houses visited:", countUniqueKeys(santa.Visited, roboSanta.Visited))
 }
 
 func main() {
