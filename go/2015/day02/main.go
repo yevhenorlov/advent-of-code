2eb3abe5f13e2ad86949889f631a4d7a9c25e689
yevhen orlov <yevhenorlov@gmail.com>
2025-06-05 21:45:55 +0200

    0202

--- go/2015/day02/main.go
+++ go/2015/day02/main.go
@@ -24,17 +24,6 @@ import (
 	"strings"
 )
 
-func getPaper(sides []int) int {
-	return 3*sides[0] + 2*sides[1] + 2*sides[2]
-}
-
-func getSides(dimensions []int) []int {
-	a := dimensions[0] * dimensions[1]
-	b := dimensions[1] * dimensions[2]
-	c := dimensions[0] * dimensions[2]
-	return []int{a, b, c}
-}
-
 func getDimensions(parts []string) []int {
 	var dimensions []int
 	for _, part := range parts {
@@ -45,6 +34,17 @@ func getDimensions(parts []string) []int {
 		dimensions = append(dimensions, v)
 	}
 	return dimensions
+}
+
+func getSides(dimensions []int) []int {
+	a := dimensions[0] * dimensions[1]
+	b := dimensions[1] * dimensions[2]
+	c := dimensions[0] * dimensions[2]
+	return []int{a, b, c}
+}
+
+func getPaper(sides []int) int {
+	return 3*sides[0] + 2*sides[1] + 2*sides[2]
 }
 
 func partOne() {
@@ -72,10 +72,54 @@ func partOne() {
 }
 
 /*
- */
+--- Part Two ---
+
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+How many total feet of ribbon should they order?
+
+*/
+
+func getVolume(dimensions []int) int {
+	return dimensions[0] * dimensions[1] * dimensions[2]
+}
+
+func getShortestPerimeter(dimensions []int) int {
+	a := (dimensions[0] + dimensions[1]) * 2
+	b := (dimensions[1] + dimensions[2]) * 2
+	c := (dimensions[0] + dimensions[2]) * 2
+	perimeters := []int{a, b, c}
+	slices.Sort(perimeters)
+	return perimeters[0]
+}
 
 func partTwo() {
-	//
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "x")
+
+		dimensions := getDimensions(parts)
+		ribbon := getShortestPerimeter(dimensions)
+		bow := getVolume(dimensions)
+		fmt.Println("dimensions:", line, "ribbon length:", ribbon+bow)
+		total = total + ribbon + bow
+	}
+	fmt.Println("total ribbon needed:", total)
+
 }
 
 func main() {
